0ec67202dcc2b253998a3e7632bfe6136b68cf31
sinist3rr <darkfloki@protonmail.com>
2020-03-09 15:06:01 +0200

    use operator module

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,25 +1,18 @@
 from random import randint
 from random import choice
+from operator import add, sub, mul
 
 
 RULE = 'What is the result of the expression?'
 
 
-def calculate(number1, sign, number2):
-    if sign == '+':
-        return number1 + number2
-    elif sign == '-':
-        return number1 - number2
-    elif sign == '*':
-        return number1 * number2
-
-
 def check():
-    string = '+-*'
-    random_sign = choice(string)
     random_number1 = randint(1, 100)
     random_number2 = randint(1, 100)
-    complete_string = (str(random_number1) + ' ' + random_sign
-                       + ' ' + str(random_number2))
-    result = calculate(random_number1, random_sign, random_number2)
+    signs_and_ops = ((add(random_number1, random_number2), '+'),
+                     (sub(random_number1, random_number2), '-'),
+                     (mul(random_number1, random_number2), '*'))
+    result, random_sign = choice(signs_and_ops)
+    complete_string = ('{} {} {}'.format(str(random_number1),
+                       random_sign, str(random_number2)))
     return (complete_string, str(result))
