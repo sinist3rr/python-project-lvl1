5db132edbd5040da0e815581de37549b2a7f6cbb
sinist3rr <darkfloki@protonmail.com>
2020-03-09 15:24:10 +0200

    no need for this module

--- brain_games/answer_checker.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-
-def run(game):
-    random_number = randint(1, 100)
-    if game(random_number):
-        return (str(random_number), 'yes')
-    elif not game(random_number):
-        return (str(random_number), 'no')
